8d0b2d33545567602a13e67630699ee3fc0e0d66
ilovewarpig <32903850+ilovewarpig@users.noreply.github.com>
2020-03-05 08:54:41 +0800

    Update Part.cs
    填补了注释

--- Part.cs
+++ Part.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,14 +6,16 @@ using System.Web;
 namespace WebApplication1
 {
     [Serializable]
+    // arduino部件的类，通过session跨页面传递识别结果.
     public class Part
     {
-        string id;
-        string name;
-        string subpath;
-        float prob;
-        string url;
+        string id;  // 标识码
+        string name;  // 部件名称
+        string subpath;  // 示例图片在本地的存储位置
+        float prob;  // 模型预测出的概率
+        string url;  // 数字资源的网址
 
+        // 设置对象属性
         public void set(string id, string name, string subpath, float prob, string url)
         {
             this.id = id;
@@ -23,6 +25,7 @@ namespace WebApplication1
             this.url = url;
         }
 
+        // 获取对象属性
         public string get_id()
         {
             return this.id;
